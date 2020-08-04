adea9497c62bb8db8ee46b36a0d36dcb8f9d3a39
RoodzFernando <50186903+RoodzFernando@users.noreply.github.com>
2020-08-04 10:21:30 -0400

    Refacor the Display component

--- src/components/Display.js
+++ src/components/Display.js
@@ -4,17 +4,16 @@ import PropTypes from 'prop-types';
 export default class Display extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      defaultResult: 0
-    }
   }
   render() {
     return (
-        <div className="result">{this.state.defaultResult}</div>
+        <div className="result">{this.props.result}</div>
     );
   }
 }
-
+Display.defaultProps = {
+  result: 0
+}
 Display.propTypes = {
   result: PropTypes.string
 }
